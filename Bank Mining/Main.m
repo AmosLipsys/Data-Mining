186cb1d15fbc806ed33b675a9cc337305c10e7e8
AmosLipsys <AmosLipsys@users.noreply.github.com>
2017-05-31 02:23:13 +1000

    Started From Scratch

---
+++ Bank Mining/Main.m
@@ -0,0 +1,23 @@
+clc
+clear
+
+file_name = 'bank-full.csv';
+
+processed_data = Preprocessing(file_name);
+Clustering(processed_data)
+
+
+
+function processed_data = Preprocessing(file_name)
+    raw_data = readtable(file_name);
+    raw_data(:,10) = [];
+    
+    processed_data = raw_data;
+end
+
+function Clustering(file_name)
+    raw_data = readtable(file_name);
+    raw_data(:,10) = [];
+    
+    processed_data = raw_data;
+end
