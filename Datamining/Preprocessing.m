f1faea55e160dd88eeaaf22b8471ee020ae5c7d2
AmosLipsys <AmosLipsys@users.noreply.github.com>
2017-05-30 20:06:42 +1000

    Prepossessing For Small Data (States) Works
    Make a table of answers with questions as the headings

--- Datamining/Preprocessing.m
+++ Datamining/Preprocessing.m
@@ -1,11 +1,58 @@
 clc;
 clear;
 
+%% Pre Processing
+
 rawdata = readtable('AmericaStateHospitalRawData.csv');
+clc;
 processesed_data = rawdata;
 processesed_data(:,3) = [];
 processesed_data(:,3) = [];
 processesed_data(:,4) = [];
+
+xx = processesed_data.State;
+states = unique(xx,'stable');
+number_of_questions= cellfun(@(x) sum(ismember(xx,x)), states,'un',0);
+number_of_questions = number_of_questions{1};
+
+
+% Edit Headings
+questions = processesed_data.HCAHPSQuestion(1:number_of_questions)';
+questions = replace(questions,'Patients who reported that ','');
+questions = replace(questions,'Patients who ','');
+questions = replace(questions,' before giving it to them','');
+questions = replace(questions,' ','_');
+questions = replace(questions,'"','');
+questions = replace(questions,',','');
+questions = replace(questions,'“','');
+questions = replace(questions,'”','');
+questions = replace(questions,'_about','');
+questions = replace(questions,'_they_were_not_given','');
+questions = replace(questions,'_they_were_given','');
+questions = replace(questions,'_the_hospital','');
+questions = replace(questions,'_when_they_left','');
+questions = replace(questions,'on_a_scale_from_0_(lowest)_to_10_(highest)','');
+questions = replace(questions,'the_area_around_','');
+
+
+% Make Table of answers with questions as headings
+answers_row = processesed_data(1 + number_of_questions - number_of_questions:number_of_questions,3);
+answers_row = table2array(answers_row);
+answers = array2table(answers_row.');
+for i = 2:size(states,1)
+    answers_row = processesed_data(1 + i*number_of_questions - number_of_questions:i*number_of_questions,3);
+    answers_row = table2array(answers_row);
+    answers_row = array2table(answers_row.');
+    answers = [answers;answers_row];
+end
+answers.Properties.VariableNames = questions;
+
+% Create New Table 
+answers.States = states;
+new_table = answers (:,[end 1:end-1]);
+
+
+
 
 
 
