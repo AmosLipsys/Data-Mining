006b25fed996a45c5bf5d613f99bae75c86eb333
AmosLipsys <AmosLipsys@users.noreply.github.com>
2017-06-02 07:00:28 +1000

    Kinda Works

--- Bank Mining/Main.m
+++ Bank Mining/Main.m
@@ -5,7 +5,15 @@ file_name = 'bank-full.csv';
 
 % PreProcessing Step
 processed_data = Preprocessing(file_name);
-clustered_data = Clustering(processed_data);
+
+% Seperate Training and Testing Data
+[training_data, testing_data] = SplitData(processed_data, percent_training);
+
+% Classifying Step
+classifed_data = ClassifyData(processed_data);
+
+% Testing Step
+results = TestingWithTable(testing_data, classifed_data);
 
 
 function processed_data = Preprocessing(file_name)
@@ -40,7 +48,7 @@ function processed_data = Preprocessing(file_name)
     
     % --BALANCE-- 
     % 1: Balance under 0, 2: Balance 0 - $1499, 3: Balance $1500+
-    processed_data.balance( 0 <= processed_data.balance & processed_data.balance < 1499 ) = 2;
+    processed_data.balance( 0 <= processed_data.balance & processed_data.balance <= 1499 ) = 2;
     processed_data.balance( processed_data.balance < 0 ) = 1;
     processed_data.balance( processed_data.balance >= 1500 ) = 3;
     
@@ -59,42 +67,123 @@ function processed_data = Preprocessing(file_name)
     
 end
 
-function clustered_data =  Clustering(table)
+function var_map  =  ClassifyData(table)
     vars_names = table.Properties.VariableNames;
     length_table = width(table);
-    height_table = height(table);
+    
     vars_names(end) = [];
     
-    
+    amount_of_data = height(table);
+    total_win = sum(table.y);
+    total_lose = sum(~table.y);
+    probability_win = total_win/amount_of_data;
+    probability_lose = total_lose/amount_of_data;
     
     % Create array of category data
+    var_map = containers.Map;
     var_array = {};
     total_percent_array = [];
     for i = (1:length_table-1)
         % Create Unique Table 
         categories = table2array(unique(table(:,i)));
-        percent_array = [];
+        categories_map = containers.Map;
         categories_array = {};
         for j = (1:size(categories))
-            var = table2array(table(:,i));
+            variable_array = table2array(table(:,i));
             category = categories(j);
             category = [category];
             if iscell(category)
                 category = string(category);
             end
                         
-            is_catagory = eq(var, category);
-            amount_cat = sum(is_catagory);
-            amount_incorrect = sum(and(is_catagory, ~table.y));      
-            percent = amount_incorrect/amount_cat;
-            categories_sub_array = [category, amount_cat, amount_incorrect];
+            is_catagory = eq(variable_array, category);
+            
+            amount_correct = sum(and(is_catagory, table.y));
+            amount_incorrect = sum(and(is_catagory, ~table.y));
+                             
+            probability_correct = amount_correct/total_win;
+            probability_incorrect = amount_incorrect/total_lose;
+            
+            if isa(category, 'double')
+                category = num2str(category);
+            else
+                category = char(category);
+            end
+            categories_map(category) = {probability_correct, probability_incorrect};
+            categories_sub_array = [probability_correct, probability_incorrect];
             categories_array = [categories_array, categories_sub_array];
-            percent_array = [percent_array, percent];
+            
         end
-        total_percent_array = [total_percent_array, [percent_array]];
+        var_map(table.Properties.VariableNames{i}) = categories_map;
         var_array = [var_array, {categories_array}];
     end
     
-       clustered_data = var_array;
+    var_map('prob_win') = probability_win;
+    var_map('prob_lose') = probability_lose;    
+end
+
+function [training, testing] = SplitData(processed_data, percent_training)
+    training = processed_data;
+    testing = processed_data;
+    traing_size = int32(height(training)*percent_training);
+            
+    testing(1:traing_size,:) = [];
+    training(traing_size:end,:) = [];
+    
+    
+end
+
+function results = TestingWithTable(testing_data, classifed_data)
+    num_of_data = height(testing_data);
+    correct = 0;
+    for i = (1:num_of_data)
+        testing_row = testing_data(i,:);
+        chance = TestOneRow(testing_row, classifed_data);
+        if testing_row.y == (chance >= .50)
+            correct = correct + 1;
+        end
+        disp(correct / i) 
+    end
+    disp(correct / num_of_data)
+        
+    results = chance;  
+end
+
+
+function probility_of_success = TestOneRow(row, classifed_data)
+    
+    number_of_vars = width(row) - 1;
+    prob_true = classifed_data('prob_win');
+    prob_false = classifed_data('prob_lose');
+    cat_prob_true = [];
+    cat_prob_false = [];
+    for i = (1:number_of_vars)
+        string = table2array(row(1,i));
+        if isa(string, 'double')
+            string = num2str(string);
+        else
+            string = char(string);
+        end
+        
+        
+        category_map = classifed_data(row.Properties.VariableNames{i});
+        try            
+            cat_probs = category_map(string);
+        catch ME
+            cat_probs = {1, 1};
+            disp(string)
+            disp(row.Properties.VariableNames{i})
+            disp(ME)
+        end
+           
+        cat_prob_true = [cat_prob_true, cat_probs(1)];
+        cat_prob_false = [cat_prob_false, cat_probs(2)];
+        
+    end
+    likelyhood_win = prod(cell2mat(cat_prob_true)) * prob_true;
+    likelyhood_lose = prod(cell2mat(cat_prob_false)) * prob_false;
+    
+    probility_of_success = likelyhood_win/(likelyhood_win + likelyhood_lose);
+       
     
 end
